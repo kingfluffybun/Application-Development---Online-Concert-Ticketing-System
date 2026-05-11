c635da1c20eddb504213556d084466725dc5499a
Fluffybun <Clarence.luna651@gmail.com>
2026-05-11 13:35:07 +0800

    added logout in homepage

--- views/index.php
+++ views/index.php
@@ -28,6 +28,7 @@ $user_name = $_SESSION['user_name'] ?? 'Guest';
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-icon lucide-circle-user"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="10" r="3"/><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"/></svg>
                     <p><?php echo htmlspecialchars($user_name); ?></p>
                 </a>
+                <?php if ($user_name !== "Guest") {echo "<a href='' class='a-text'><p>Logout</p></a>";} ?>
             </div>
         </div>
         <div class="hero-banner-container">
