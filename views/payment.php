053fa8c021d98d049538d428cd355988dc8abd3a
Fluffybun <Clarence.luna651@gmail.com>
2026-05-12 12:16:30 +0800

    fixed path directory

--- views/payment.php
+++ views/payment.php
@@ -30,7 +30,7 @@ $basePrice = $zonePrices[$zone] ?? 0;
 $totalPrice = $basePrice * $quantity;
 ?>
     <section class="ticket-form-section">
-    <a href="/views/seats.php">
+    <a href="/views/">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-left-icon lucide-move-left"><path d="M6 8L2 12L6 16"/><path d="M2 12H22"/></svg>
         <p>Back to Home</p>
     </a>
