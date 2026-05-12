f4f68cb7e4e288b2fd1f636e5162c2d8fb687ae0
Fluffybun <Clarence.luna651@gmail.com>
2026-05-13 00:11:28 +0800

    may nakaligtaan

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ session_start();
                 <p>Experience BLACKPINK live in Quezon City — </p>
                 <p>the most iconic K-pop girl group takes the stage at Smart Araneta Coliseum</p>
             </div>
-            <a href="/views/seats/seats.php"><p>Get Tickets Now</p><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></a>
+            <a href="/views/seats.php"><p>Get Tickets Now</p><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></a>
         </div>
     </hero>
     <!-- <div class="ticket-pricing-banner"></div> -->
