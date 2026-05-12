703e555dee21ebe3dd42561edd38ec403a318a01
Fluffybun <Clarence.luna651@gmail.com>
2026-05-13 00:43:53 +0800

    change db.php to local

--- db/db.php
+++ db/db.php
@@ -1,15 +1,15 @@
 <?php
 // For Local
-// $host = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "ticketsystem";
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "ticketsystem";
 
 // For Online
-$host = "sql202.yzz.me";
-$username = "yzzme_41900988";
-$password = "L7kZuJRcmgeY";
-$database = "yzzme_41900988_ticketsystem";
+// $host = "sql202.yzz.me";
+// $username = "yzzme_41900988";
+// $password = "L7kZuJRcmgeY";
+// $database = "yzzme_41900988_ticketsystem";
  
 $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
